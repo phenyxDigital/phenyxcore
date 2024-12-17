bfe5e3ae2cb24cd6b8586c35d77791e8083f270d
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2024-12-17 15:34:44 +0100

    1.9.7

--- src/classes/Tools.php
+++ src/classes/Tools.php
@@ -6187,6 +6187,39 @@ FileETag none
             ],
             $input);
     }
+    
+    public static function getHolidays($year = null) {
+
+		if ($year === null) {
+			$year = intval(strftime('%Y'));
+		}
+
+		$easterDate = easter_date($year);
+		$easterDay = date('j', $easterDate);
+		$easterMonth = date('n', $easterDate);
+		$easterYear = date('Y', $easterDate);
+
+		$holidays = [
+			// Jours feries fixes
+			mktime(0, 0, 0, 1, 1, $year), // 1er janvier
+			mktime(0, 0, 0, 5, 1, $year), // Fete du travail
+			mktime(0, 0, 0, 5, 8, $year), // Victoire des allies
+			mktime(0, 0, 0, 7, 14, $year), // Fete nationale
+			mktime(0, 0, 0, 8, 15, $year), // Assomption
+			mktime(0, 0, 0, 11, 1, $year), // Toussaint
+			mktime(0, 0, 0, 11, 11, $year), // Armistice
+			mktime(0, 0, 0, 12, 25, $year), // Noel
+
+			// Jour feries qui dependent de paques
+			mktime(0, 0, 0, $easterMonth, $easterDay + 1, $easterYear), // Lundi de paques
+			mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear), // Ascension
+			mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear), // Pentecote
+		];
+
+		sort($holidays);
+
+		return $holidays;
+	}
 
 
 }
