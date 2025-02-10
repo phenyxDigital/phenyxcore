3ad48c2ce5d9e2a34830fba753f29d6268bc92f8
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2025-02-10 11:18:35 +0100

    1.9.7

--- src/classes/shortcodes/rev_slider.php
+++ src/classes/shortcodes/rev_slider.php
@@ -1,3 +1,10 @@
 <?php
-
-echo '[rev_slider alias="'.$atts['alias'].'"]';
+extract(Composer::shortcode_atts([
+    'alias' => '', //standard, button_count, box_count
+    'el_class'    => '',
+], $atts));
+$el_class = $this->getExtraClass($el_class);
+$output = '<div class="' . $el_class . '>';
+$output .= '[rev_slider alias="'.$alias.'"]';
+$output .= '</div>';
+echo $output;
