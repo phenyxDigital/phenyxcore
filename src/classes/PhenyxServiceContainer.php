8c7eea7ba2e83c4f24700db5acf7e6dac76b6263
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2025-03-06 13:42:16 +0100

    2.0.1

---
+++ src/classes/PhenyxServiceContainer.php
@@ -0,0 +1,84 @@
+<?php
+
+class PhenyxServiceContainer {
+    
+    private static $instance = null;
+
+    private $app = null;
+    
+    private $services = [];
+    
+    private $constructors = [];
+
+    // Constructor
+
+    private function __construct() {
+        
+        self::$instance = $this;
+    }
+    
+    public static function getInstance() {
+
+		if (!isset(static::$instance)) {
+			static::$instance = new ServiceContainer();
+		}
+
+		return static::$instance;
+	}
+
+
+    // Methods
+
+    public function setApp($app) {
+        
+        $this->app = $app;
+    }
+
+    public function getApp() {
+        return $this->app;
+    }
+
+    public function get($id) {
+        // Create the service if not already availabe
+
+        if (!isset($this->services[$id]) && isset($this->constructors[$id])) {
+
+            if ($this->constructors[$id][1]) {
+                $this->services[$id] = new $this->constructors[$id][0]($this->constructors[$id][1]);
+            } else {
+                $this->services[$id] = new $this->constructors[$id][0];
+            }
+
+            $this->services[$id]->setServiceContainer($this);
+        }
+
+        // Return the service
+
+        return $this->services[$id];
+    }
+
+    public function registerService($id, $ServiceClass, $opts = []) {
+        // Store the service constructor
+
+        $this->constructors[$id] = [$ServiceClass, $opts];
+    }
+
+    public function registerServiceInstance($id, $instance) {
+        // Store the service instance
+
+        $this->services[$id] = $instance;
+    }
+
+    public function clean() {
+        // Remove the registered services
+
+        foreach ($this->services as $service) {
+            $service->onRemove();
+        }
+
+        $this->services = [];
+    }
+
+}
+
+?>
---
+++ src/classes/PhenyxServices.php
@@ -0,0 +1,58 @@
+<?php
+
+// Base class for all services implementing the Singleton pattern
+
+class PhenyxServices extends stdClass {
+    // Fields
+
+    protected $services;
+    
+    public $context;
+    
+    public function __construct() {
+
+        
+        $this->context = Context::getContext();
+
+        if (!isset($this->context->phenyxConfig)) {
+            $this->context->phenyxConfig = Configuration::getInstance();
+
+        }
+
+        if (!isset($this->context->_tools)) {
+            $this->context->_tools = PhenyxTool::getInstance();
+        }
+
+        if (!isset($this->context->language)) {
+            $this->context->language = $this->context->_tools->jsonDecode($this->context->_tools->jsonEncode(Language::buildObject($this->context->phenyxConfig->get('EPH_LANG_DEFAULT'))));
+        }
+
+
+    }
+
+
+    // Methods
+
+    public function setServiceContainer($container) {
+        
+        $this->services = $container;
+
+        $this->onRegister();
+    }
+
+    public function onRegister() {}
+
+    public function onRemove() {}
+
+    public function get($serviceId) {
+        
+        return $this->services->get($serviceId);
+    }
+
+    public function getApp() {
+        
+        return $this->services->getApp();
+    }
+}
+
+?>
